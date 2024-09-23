c84627990e849de559f59c7137f00f62fbec81c4
Hristo Georgiev <georgievh@yahoo.co.uk>
2024-09-23 15:23:55 +0100

    [ODPR-1529] - Contact details journey tests.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,9 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
+    "ch.qos.logback"       % "logback-classic" % "1.5.6"  % Test,
+    "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8" % Test,
+    "org.scalatest"       %% "scalatest"       % "3.2.19" % Test,
     "uk.gov.hmrc"         %% "ui-test-runner"  % "0.38.0" % Test
   )
-
 }
---
+++ src/test/scala/uk/gov/hmrc/ui/specs/contactdetails/IndividualJourneysSpec.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.specs.contactdetails
+
+import support.BaseSpec
+import support.builders.UserCredentialsBuilder.anIndividualUser
+import uk.gov.hmrc.ui.pages.contactdetails.ContactDetailsPage
+import uk.gov.hmrc.ui.pages.contactdetails.individual._
+import uk.gov.hmrc.ui.pages.{AuthLoginStubPage, IndexPage, ResultPage}
+
+class IndividualJourneysSpec extends BaseSpec {
+
+  private val loginPage                         = AuthLoginStubPage
+  private val indexPage                         = IndexPage()
+  private val contactDetailsPage                = ContactDetailsPage()
+  private val individualEmailAddressPage        = IndividualEmailAddressPage()
+  private val individualEmailAddressUpdatedPage = IndividualEmailAddressUpdatedPage()
+  private val canPhoneIndividualPage            = CanPhoneIndividualPage()
+  private val individualPhoneNumberRemovedPage  = IndividualPhoneNumberRemovedPage()
+  private val individualPhoneNumberPage         = IndividualPhoneNumberPage()
+  private val individualPhoneNumberUpdatedPage  = IndividualPhoneNumberUpdatedPage()
+  private val resultPage                        = ResultPage
+
+  Feature("Individual Journeys") {
+    Scenario("Change Contact Details") {
+      Given("Individual user logs in")
+      loginPage.show()
+      loginPage.loginAs(anIndividualUser)
+
+      When("Change contact details is clicked")
+      indexPage.clickChangeContactDetails()
+
+      And("Email address updated")
+      contactDetailsPage.clickChangeEmail()
+      individualEmailAddressPage.withEmail("marjorie.simpson.1@example.com").continue()
+      individualEmailAddressUpdatedPage.continue()
+
+      And("Can we contact you by telephone and phone number are updated")
+      contactDetailsPage.clickChangeCanWeContactYouByPhone()
+      canPhoneIndividualPage.selectNo().continue()
+      individualPhoneNumberRemovedPage.continue()
+      contactDetailsPage.clickChangeCanWeContactYouByPhone()
+      canPhoneIndividualPage.selectYes().continue()
+      individualPhoneNumberPage.withPhoneNumber("0200000002").continue()
+      individualPhoneNumberUpdatedPage.continue()
+
+      And("Your contact details 'Back' is clicked")
+      contactDetailsPage.continue()
+
+      Then("The result page should be 'Manage your Digital Platform Reporting'")
+      resultPage.url       should include("/manage-reporting")
+      resultPage.heading shouldBe "Manage your Digital Platform Reporting"
+    }
+  }
+}
