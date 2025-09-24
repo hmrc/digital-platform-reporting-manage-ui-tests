7d7bda9d12520b0da4e31299f2cc0305ee2556d5
swetaoza16 <sweta.oza@digital.hmrc.gov.uk>
2025-09-24 12:42:28 +0100

    DLS-12262 UI Test Runner version update to latest

--- run_operator_tests.sh
+++ run_operator_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BROWSER=${1:-chrome}
+BROWSER=${1:-firefox}
 HEADLESS=${2:-true}
 
 sbt clean -Dbrowser="${BROWSER}" -Dbrowser.option.headless="${HEADLESS}" "testOnly uk.gov.hmrc.ui.specs.* -- -n support.tags.OperatorFeature" testReport
