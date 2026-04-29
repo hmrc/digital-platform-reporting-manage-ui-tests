ad14dd8955c35ead1375b8ab3c8b55af03e1b69f
gajalakshmiseetharaman <177637840+gajalakshmiseetharaman@users.noreply.github.com>
2026-04-29 16:43:25 +0100

    DLS-12964|Fixed intermittent jenkins failure.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "ch.qos.logback"       % "logback-classic"         % "1.5.26" % Test,
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8" % Test,
     "org.scalatest"       %% "scalatest"               % "3.2.19" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner"          % "0.52.0" % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"          % "0.54.0" % Test,
     "org.mongodb"          % "mongodb-driver-sync"     % "5.6.2"  % Test,
     "com.typesafe.play"   %% "play-json"               % "2.10.8" % Test,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-30" % "2.12.0" % Test
